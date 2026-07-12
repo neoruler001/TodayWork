eddae5fdfc9c1bf982ee6d412caab99c3f392e3f
A402877 <neoemperor@bp.hd.com>
2026-07-13 07:46:44 +0900

    위젯 수정2

--- app/src/main/java/com/todaywork/app/widget/CalendarWidget.kt
+++ app/src/main/java/com/todaywork/app/widget/CalendarWidget.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.Composable
 import androidx.glance.*
 import androidx.glance.action.actionParametersOf
+import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.appwidget.action.actionStartActivity
